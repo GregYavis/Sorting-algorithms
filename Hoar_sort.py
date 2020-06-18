b91f01cd328f7b6d0048d931cdf59788fdf5c799
Gregory Kozlov <gregyavis@gmail.com>
2020-06-18 14:30:42 +0300

    Update Hoar_sort.py

--- Hoar_sort.py
+++ Hoar_sort.py
@@ -1,1 +1,21 @@
 def quick_sort(arr):
+  if len(arr)<=1:
+    return
+  else:
+    barrier = arr[0]
+    L = []
+    R = []
+    M = []
+    for i in arr:
+      if i < barrier:
+        L.append(i)
+      elif i == barrier:
+        M.append(i)
+      else:
+        R.append(i)
+    quick_sort(L)
+    quick_sort(R)
+    i = 0
+    for digit in L+M+R:
+      arr[i] = digit
+      i += 1
