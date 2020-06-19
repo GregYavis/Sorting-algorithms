8647f0555037c7b64973800c44ac1fcc98f2c3b9
Gregory Kozlov <gregyavis@gmail.com>
2020-06-19 18:19:00 +0300

    Create Merge_sort.py

---
+++ Merge_sort.py
@@ -0,0 +1,31 @@
+def merhr_sort(arr):
+  if len arr>1:
+    midle = len(arr)//2
+    L = arr[:middle]
+    R = [middle:]
+    merge_sort(L)
+    merge_sort(R)
+    
+    i=0
+    j=0
+    k=0
+    
+    while i < len(L) and j < len(R):
+      if L[i] < R[j]:
+        arr[k] = L[i]
+        i += 1
+      else:
+        arr[k] = R[j]
+        j += 1
+      k += 1
+      
+      while i<len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+        
+      while j<len(R):
+      arr[k] = R[j]
+      j += 1
+      k += 1
+      
